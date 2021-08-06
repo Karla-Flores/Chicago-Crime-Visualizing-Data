5ccbca184e0f8945a0245b2a8e1e7b9c181afac9
uldom <muldom@gmail.com>
2021-08-06 17:23:54 -0500

    Project_03_UD updated and table created wuth SQLAlchemy

---
+++ Resources/Create_Table_Qry.sql
@@ -0,0 +1,21 @@
+-- Drop if table exists
+DROP TABLE IF EXISTS "Chicago_crime";
+
+-- Create the table
+CREATE TABLE "Chicago_crime" (
+    "ID" serial PRIMARY KEY,
+    "Date" VARCHAR,
+	"IUCR" VARCHAR,
+	"Primary Type" VARCHAR,
+	"Description" VARCHAR,
+	"Location Description" VARCHAR,
+	"Arrest" BOOLEAN,
+	"Domestic" BOOLEAN,
+	"Year" INT,
+	"Latitude" FLOAT,
+	"Longitude" FLOAT
+)
+;
+
+SELECT * FROM "Chicago_crime"
+
