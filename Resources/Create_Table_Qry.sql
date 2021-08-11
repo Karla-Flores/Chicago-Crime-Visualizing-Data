9cfe1a32e10d0eccf618e0d37fee4952216c33c4
Karla-Flores <ms.kaflores@gmail.com>
2021-08-11 16:52:15 -0500

    SQL table updated

--- Resources/Create_Table_Qry.sql
+++ Resources/Create_Table_Qry.sql
@@ -20,22 +20,31 @@ CREATE TABLE chicago_crime (
 
 SELECT * FROM chicago_crime;
 
-SELECT "year", "month", "primary_type", COUNT("primary_type")
-FROM chicago_crime
-GROUP BY "year", "month", "primary_type"
-ORDER BY "year" DESC;
 
-SELECT "year", "month", "primary_type", "description","arrest", COUNT("Arrest")
-FROM chicago_crime
-GROUP BY "year", "month", "primary_type", "description","arrest"
-ORDER BY "year" DESC;
-
+-- Chicago crime month evolution 
 SELECT "year", "month", "primary_type", "description", COUNT("primary_type")
 FROM chicago_crime
 GROUP BY "year", "month", "primary_type", "description"
 ORDER BY "year" DESC;
 
+
+-- Chicago crime month evolution  - arrest ratio --
+SELECT "year", "month", "primary_type", "description", "arrest", COUNT("arrest") AS "Arrest Ratio"
+FROM chicago_crime
+GROUP BY "year", "month", "primary_type", "description","arrest"
+ORDER BY "year" DESC;
+
+
+-- Location desciption 
 SELECT "year", "month", "primary_type", "description", "location_description", COUNT("description")
 FROM chicago_crime
 GROUP BY "year", "month", "primary_type", "description", "location_description"
 ORDER BY "year" DESC;
+
+
+-- Primary type and description
+SELECT "year", "month", "primary_type", "description", COUNT("description")
+FROM chicago_crime
+GROUP BY "year", "month", "primary_type", "description"
+ORDER BY "year" DESC;
+
