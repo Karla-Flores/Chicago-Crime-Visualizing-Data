f9731aa9b0ad4a9c7867930bbb6ff3745f33e4ae
My Name <harika.raj@hotmail.com>
2021-08-11 01:40:36 -0500

    added two api calls

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
+from sqlalchemy.sql.expression import distinct
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 
 #################################################
@@ -15,7 +15,7 @@ rds_connection_string = "postgres:postgres@localhost:5432/Project_03"
 engine = create_engine(f'postgresql://{rds_connection_string}')
 Base = automap_base()
 Base.prepare(engine, reflect=True)
-db = Base.classes.Chicago_crime
+db = Base.classes.chicago_crime
 
 print(engine.table_names())
 
@@ -29,55 +29,57 @@ app = Flask(__name__)
 # Flask Routes
 # #################################################
 
+
 @app.route("/")
-def welcome():
-    """List all available api routes."""
-    return (
-        f"Available Routes:<br/>"
-        f"/api/v1.0/Year<br/>"
-        f"/api/v1.0/Primary_Type"
-    )
-
-
-@app.route("/api/v1.0/Year")
-def years():
+def home():
 #     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-#     """Return a list of all passenger names"""
-#     # Query all passengers
-    results = session.query("Chicago_crime"."Year").all()
+    results_y = session.query(db.year).group_by(db.year)
+    results_d = session.query(db.primary_type).group_by(db.primary_type)
+    results_x = session.query(db.description).group_by(db.description)
 
     session.close()
 
-#     # Convert list of tuples into normal list
-    all_years = list(np.ravel(results))
+    return render_template('index.html', Year = [result[0] for result in results_y], Primary_Type = [result[0] for result in results_d],Description = [result[0] for result in results_x])
 
-    return jsonify(all_years)
+@app.route("/api")
+
+def api_list():
+
+    """List all available api routes."""
+
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/year<br/>"
+        f"/api/v1.0/year/primary_type"
+    )
 
 
-# @app.route("/api/v1.0/passengers")
-# def passengers():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
+@app.route('/api/v1.0/<year>')
 
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-#     results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+def filter_year(year):
 
-#     session.close()
+    session = Session(engine)
+    
+    result = session.query(db.primary_type, func.count(db.description)).group_by(db.year, db.primary_type).filter(db.year == year).order_by(func.count(db.description).desc()).all()
 
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_passengers = []
-#     for name, age, sex in results:
-#         passenger_dict = {}
-#         passenger_dict["name"] = name
-#         passenger_dict["age"] = age
-#         passenger_dict["sex"] = sex
-#         all_passengers.append(passenger_dict)
+    session.close()
+    
+    return jsonify(result)
 
-#     return jsonify(all_passengers)
 
+@app.route('/api/v1.0/<year>/<primary_type>')
+
+def filter_yr_type(year, primary_type):
+
+    session = Session(engine)
+
+    result = session.query(db.description, func.count(db.description)).group_by(db.year, db.primary_type, db.description).filter(db.year == year).filter(db.primary_type == primary_type).order_by(func.count(db.description).desc()).all()
+
+    session.close()
+
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
