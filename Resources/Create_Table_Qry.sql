f1130eb6b792d17dc79cca7538912df22765c092
uldom <muldom@gmail.com>
2021-08-11 19:28:04 -0500

    qry updated

--- Resources/Create_Table_Qry.sql
+++ Resources/Create_Table_Qry.sql
@@ -22,29 +22,37 @@ SELECT * FROM chicago_crime;
 
 
 -- Chicago crime month evolution 
-SELECT "year", "month", "primary_type", "description", COUNT("primary_type")
+SELECT year, month, primary_type, description, COUNT(primary_type)
 FROM chicago_crime
-GROUP BY "year", "month", "primary_type", "description"
-ORDER BY "year" DESC;
+GROUP BY year, month, primary_type, description
+ORDER BY year DESC;
 
 
--- Chicago crime month evolution  - arrest ratio --
-SELECT "year", "month", "primary_type", "description", "arrest", COUNT("arrest") AS "Arrest Ratio"
+-- Chicago crime month evolution  - total arrests -- Only Primary Type
+SELECT year, primary_type, arrest, COUNT(arrest) AS total
 FROM chicago_crime
-GROUP BY "year", "month", "primary_type", "description","arrest"
-ORDER BY "year" DESC;
+where year = 2016
+GROUP BY year, primary_type, arrest
+ORDER BY year DESC;
+
+
+-- Chicago crime month evolution  - total arrests --
+SELECT year, primary_type, description, arrest, COUNT(arrest) AS arrest_ratio
+FROM chicago_crime
+GROUP BY year, primary_type, description, arrest
+ORDER BY year DESC;
 
 
 -- Location desciption 
-SELECT "year", "month", "primary_type", "description", "location_description", COUNT("description")
+SELECT year, month, primary_type, description, location_description, COUNT(description)
 FROM chicago_crime
-GROUP BY "year", "month", "primary_type", "description", "location_description"
-ORDER BY "year" DESC;
+GROUP BY year, month, primary_type, description, location_description
+ORDER BY year DESC;
 
 
 -- Primary type and description
-SELECT "year", "month", "primary_type", "description", COUNT("description")
+SELECT year, month, primary_type, description, COUNT(description)
 FROM chicago_crime
-GROUP BY "year", "month", "primary_type", "description"
-ORDER BY "year" DESC;
+GROUP BY year, month, primary_type, description
+ORDER BY year DESC;
 
